cb96a2155f840f5f4a2d0de123cc48541355c88c
tonythedealer <yuliyanasher@gmail.com>
2016-04-15 20:09:40 +0200

    Update RECONNAISSANCE.py

--- RECONNAISSANCE.py
+++ RECONNAISSANCE.py
@@ -77,42 +77,42 @@ def final_detection(chaine):
     _de = difflib.SequenceMatcher(None,global_list[0],top_words)
     de = _de.ratio() * 100
     de_ =  { de : 'Allemand'}
-    print (de, '% of', _nameOf('de'))
+    print (de, '%', _nameOf('de'))
 
     _es = difflib.SequenceMatcher(None,global_list[1],top_words)
     es = _es.ratio() * 100
     es_ = { es : 'Espagnol'}
-    print (es, _nameOf('es'))
+    print (es,'%', _nameOf('es'))
 
     _fr = difflib.SequenceMatcher(None,global_list[2],top_words)
     fr = _fr.ratio() * 100
     fr_ = { fr : 'Français'}
-    print (fr, _nameOf('fr'))
+    print (fr,'%', _nameOf('fr'))
 
     _it = difflib.SequenceMatcher(None,global_list[3],top_words)
     it = _it.ratio() * 100
     it_ = { it : 'Italien'}
-    print (it, _nameOf('it'))
+    print (it,'%', _nameOf('it'))
 
     _sv = difflib.SequenceMatcher(None,global_list[4],top_words)
     sv = _sv.ratio() * 100
     sv_ = { sv : 'Suédois'}
-    print (sv, _nameOf('sv'))
+    print (sv,'%', _nameOf('sv'))
 
     _en = difflib.SequenceMatcher(None,global_list[5],top_words)
     en = _en.ratio() * 100
     en_ = { en : 'Anglais'}
-    print (en, _nameOf('en'))
+    print (en,'%', _nameOf('en'))
 
     _nl = difflib.SequenceMatcher(None,global_list[6],top_words)
     nl = _nl.ratio() * 100
     nl_ = { nl : 'Néerlandais'}
-    print (nl, _nameOf('nl'))
+    print (nl,'%', _nameOf('nl'))
 
     _fi = difflib.SequenceMatcher(None,global_list[7],top_words)
     fi = _fi.ratio() * 100
     fi_ = { fi : 'Finnois'}
-    print (fi, _nameOf('fi'))       
+    print (fi,'%', _nameOf('fi'))       
 
     
     if de > it and de > fr and de > es and de > sv and de > en and de > nl and de > fi:
